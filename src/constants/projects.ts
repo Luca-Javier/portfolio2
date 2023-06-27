eb9726d1d411f5ba90f08f2b906face8136a2db1
Luca-Javier <luca.jav45@gmail.com>
2023-06-26 22:10:39 -0300

    commit with gitpush!

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -5,7 +5,7 @@ export const projects: IProject[] = [
     title: "Roulette Hero",
     description:
       "Un juego web Dungeon donde todo depende de la suerte y tus decisiones.",
-    image: "/roulette-hero.png",
+    image: "/roulette-hero.webp",
     source: "https://github.com/Luca-Javier/Roulette-Hero",
     link: "https://roulette-hero.vercel.app",
     technologies: ["react", "javascript", "figma", "redux", "i18n"],
@@ -21,7 +21,7 @@ export const projects: IProject[] = [
     title: "NEXT-xckd",
     description:
       "Recopilación de comics de xckd, comics para programadores, con un escrapper en NodeJS y frontend echo en NextJS",
-    image: "/xkcd.png",
+    image: "/xkcd.webp",
     link: "https://next-comics-xkcd.vercel.app/es",
     source: "https://github.com/Luca-Javier/NEXT-xckd",
     technologies: ["react", "nextjs", "nodejs", "i18n"],
@@ -30,7 +30,7 @@ export const projects: IProject[] = [
     title: "What is GPT-3",
     description:
       "Diseño moderno sobre que es GPT-3, armado tal cual el diseño en figma",
-    image: "/modern.png",
+    image: "/modern.webp",
     link: "https://what-gpt-3-design.vercel.app",
     source: "https://github.com/Luca-Javier/whatGPT3-design",
     technologies: ["react", "sass", "figma"],
@@ -39,7 +39,7 @@ export const projects: IProject[] = [
     title: "MS-BUY",
     description:
       "Eccomerce de venta de ropa, tecnología y joyería, con un diseño moderno y minimalista. Echo con NextJS y Sanity para el CMS",
-    image: "/ms-buy.png",
+    image: "/ms-buy.webp",
     link: "https://ms-buy.vercel.app",
     source: "https://github.com/Luca-Javier/ms-buy",
     technologies: ["react", "nextjs", "typescript", "chakraui"],
@@ -47,7 +47,7 @@ export const projects: IProject[] = [
   {
     title: "Portfolio",
     description: "El portfolio que estás viendo ahora mismo",
-    image: "/portfolio.png",
+    image: "/portfolio.webp",
     source: "https://github.com/Luca-Javier/portfolio2",
     technologies: ["react", "nextjs", "styled-components", "typescript"],
   },
